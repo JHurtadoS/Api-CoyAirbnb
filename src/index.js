e25236031798539e7d00194607448acaa67e98f4
juanH <86845748+JHurtadoS@users.noreply.github.com>
2024-05-22 05:38:40 +0000

    a

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from "express";
+import pkg from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const { urlencoded } = pkg;
+import { query } from "../db/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  console.log("Hi");
-  res.sendFile(__dirname + "/index.html");
+app.use(urlencoded({ extended: true }));
+dotenv.config();
+app.get("/reservations/all", async (req, res) => {
+  try {
+    const result = await query("SELECT * FROM public.reservations");
+    console.log(result);
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Error ejecutando la consulta", err.stack);
+    res.status(500).send("Error obteniendo las reservas");
+  }
 });
 
-app.listen(8080, function () {
+app.listen(8080, () => {
   console.log(__dirname);
-
   console.log("Server is running on 8080");
 });
