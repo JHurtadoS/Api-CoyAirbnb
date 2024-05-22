2e5f25b120446e460639e930baa48d6aa2b2e348
Juan Esteban Hurtado <jhurtados@car.gov.co>
2024-05-22 10:48:24 -0500

    aja

--- src/index.js
+++ src/index.js
@@ -4,23 +4,27 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const { urlencoded } = pkg;
-import { query } from "../db/db.js";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import {createClient} from '@supabase/supabase-js'
+
+
 
 dotenv.config();
 const app = express();
 
 app.use(urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 dotenv.config();
-app.get("/reservations/all", async (req, res) => {
-  try {
-    const result = await query("SELECT * FROM public.reservations");
-    console.log(result);
-    res.json(result.rows);
-  } catch (err) {
-    console.error("Error ejecutando la consulta", err.stack);
-    res.status(500).send("Error obteniendo las reservas");
-  }
+
+const supabase = createClient(process.env.URL,process.env.API_KEY );
+
+app.get('/rooms/all', async (req, res) => {
+    const {data, error} = await supabase
+        .from('rooms')
+        .select()
+    res.send(data);
 });
 
 app.listen(8080, () => {
